3ec515f348018c0ba52aa21bee0fcb133ab86a59
xykwin <kunghsu@qq.com>
2020-02-27 21:07:05 +0800

    新增演示demo--验证MDC的线程复用性问题

--- src/main/java/cn/com/kun/controller/HelloController.java
+++ src/main/java/cn/com/kun/controller/HelloController.java
@@ -1,31 +1,9 @@
 package cn.com.kun.controller;
 
-import cn.com.kun.phoenix.NodeFlagHolder;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    private String old_node_string = "";
-    private String new_node_string = "";
-
-    //全局只需要一个
-    private NodeFlagHolder nodeFlagHolder = new NodeFlagHolder();
-
-    @RequestMapping("/hello")
-    public String test1(){
-
-        return "hello!!!";
-    }
-
-    @RequestMapping("/test2")
-    public String test2(){
-
-        nodeFlagHolder.init(System.currentTimeMillis() + "");
-        System.out.println(String.format("当前NodeFlagHolder： 旧key: %s 新key: %s",
-                nodeFlagHolder.getLastNodeKey(), nodeFlagHolder.getCurrentNodeKey()));
-        return "test2!!!";
-    }
 
 }
