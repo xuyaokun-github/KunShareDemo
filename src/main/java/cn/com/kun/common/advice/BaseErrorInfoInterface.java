a3c56ab22920b2b824ba185bd0c21c0189f86955
xuyaokun_kzx <xuyaokun_kzx@alibaba.com>
2021-05-14 18:00:15 +0800

    1.新增统一异常处理

---
+++ src/main/java/cn/com/kun/common/advice/BaseErrorInfoInterface.java
@@ -0,0 +1,9 @@
+package cn.com.kun.common.advice;
+
+public interface BaseErrorInfoInterface {
+    /** 错误码*/
+    String getResultCode();
+
+    /** 错误描述*/
+    String getResultMsg();
+}
---
+++ src/main/java/cn/com/kun/common/advice/GlobalExceptionHandler.java
@@ -0,0 +1,63 @@
+package cn.com.kun.common.advice;
+
+import cn.com.kun.common.exception.BizException;
+import cn.com.kun.common.vo.ResultVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 统一的异常处理器
+ * author:xuyaokun_kzx
+ * date:2021/5/14
+ * desc:
+*/
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    /**
+     * 处理自定义的业务异常
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value = BizException.class)
+    @ResponseBody
+    public ResultVo bizExceptionHandler(HttpServletRequest req, BizException e){
+        logger.error("发生业务异常！原因是：{}",e.getErrorMsg());
+        return ResultVo.error(e.getErrorCode(), e.getErrorMsg());
+    }
+
+    /**
+     * 处理空指针的异常
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value =NullPointerException.class)
+    @ResponseBody
+    public ResultVo exceptionHandler(HttpServletRequest req, NullPointerException e){
+        logger.error("发生空指针异常！原因是:",e);
+        return ResultVo.error(CommonEnum.BODY_NOT_MATCH);
+    }
+
+
+    /**
+     * 处理其他异常
+     * @param req
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(value =Exception.class)
+    @ResponseBody
+    public ResultVo exceptionHandler(HttpServletRequest req, Exception e){
+        logger.error("未知异常！原因是:", e);
+        return ResultVo.error(CommonEnum.INTERNAL_SERVER_ERROR);
+    }
+}
