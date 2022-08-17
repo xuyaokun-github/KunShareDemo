c59de9b0191e07913e63441daf484cf8c2f33165
xuyaokun_kzx <xuyaokun_kzx@alibaba.com>
2022-08-17 19:42:45 +0800

    feat:可传参的返回码优化

--- src/main/java/cn/com/kun/bean/api/ResultCode.java
+++ src/main/java/cn/com/kun/bean/api/ResultCode.java
@@ -14,7 +14,7 @@ public enum ResultCode {
     VALIDATE_FAILED("1002", "参数校验失败"),
     //返回上游的消息能尽量具体，能体现出是哪个参数导致了问题，例如
     STUDENT_NOT_FOUND("001001", "学生ID【%s】不存在，请检查"),
-    STUDENT_AND_CLASS_NOT_FOUND("001001", "学生ID【%s】和课程ID【%s】均不存在，请检查"),
+    STUDENT_AND_CLASS_NOT_FOUND("001002", "学生ID【%s】和课程ID【%s】均不存在，请检查"),
     ERROR("999999", "未知错误");
 
     private String code;
